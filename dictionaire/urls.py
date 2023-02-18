28405303cb13f6ec2b7cf666713fd1c4df40e7e0
Emane Bile Felicien Davy <82854992+davyemane@users.noreply.github.com>
2023-02-18 15:55:32 +0100

    Add files via upload

--- dictionaire/urls.py
+++ dictionaire/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path 
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path("", views.SearchListView.as_view(), name="search")
+    path("", SearchListView.as_view(), name="search")
 ]
 
